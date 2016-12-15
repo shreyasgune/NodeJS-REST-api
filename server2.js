8354fa990b8bc0cbfd7e6c08906361b8f6a0fa34
Shreyas Gune <shreyas.enug@gmail.com>
2016-12-15 12:14:38 -0500

    Initial files

---
+++ server2.js
@@ -0,0 +1,27 @@
+//Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//MongoDB
+mongoose.connect('mongodb://localhost/rest_test');
+
+//Express
+var app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+    res.send('Rest API under construction');
+});
+
+
+//Routes
+app.use('/api', require('./routes/api'));
+
+
+
+//Start Server
+app.listen(3100);
+console.log('API is running on 3100');
+
