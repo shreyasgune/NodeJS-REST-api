d3816d5589e3a2687ae85d8340b2248ae2eb1959
Shreyas Gune <sgune@gannett.com>
2017-05-25 16:50:49 -0400

    Create server3.js

---
+++ server3.js
@@ -0,0 +1,15 @@
+//Dependencies
+var express = require('express');
+
+//Express
+var app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+    res.send('Rest API under construction');
+});
+
+//Start Server
+app.listen(3100);
+console.log('API is running on 3100');
