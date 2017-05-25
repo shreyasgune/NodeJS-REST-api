12dfdd6d9c8dc36b0334c440f9f655e15b72d720
Shreyas Gune <sgune@gannett.com>
2017-05-25 17:50:24 -0400

    Update server3.js

--- server3.js
+++ server3.js
@@ -1,10 +1,10 @@
 //Dependencies
 var express = require('express');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 //Express
 var app = express();
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended : true}));
+//app.use(bodyParser.json());
 
 app.get('/', function(req,res){
     res.send('Rest API under construction');
